4e865e36bdca28c718908032ac037b96f8d8fb6b
hyuneeee1 <hyuni028@naver.com>
2022-07-12 11:13:41 +0900

    111

--- src/main/java/com/docmall/dto/EmailDTO.java
+++ src/main/java/com/docmall/dto/EmailDTO.java
@@ -16,5 +16,8 @@ public class EmailDTO {
 		this.senderMail = "livingmade"; //GMail로 사용(SMTP메일서버 이용. 현업에서는 회사 메일서버 정보를 이용해야 함.
 		this.subject = "livingmade 회원가입 메일인증코드입니다.";
 		this.message = "인증코드를 확인하시고 인증코드를 이메일 인증코드란에 입력하시기 바랍니다.";
+		
+		
+		
 	}
 }
